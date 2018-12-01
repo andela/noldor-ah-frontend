74a5dbc0d51996af71b399ea1644de51905305bc
uwaelpis <ingenioushope@gmail.com>
2018-12-02 00:58:45 +0100

    chore(setup): Setup Redux     - install react, react-redux dependencies     - install thunk and redux-immutable-state-invariant middlewares     - setup redux store     - setup actions and reducers folders
        [Delivers #161290849]

--- src/app.js
+++ src/app.js
@@ -1,6 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { simpleAction } from './actions/simpleAction';
 
-const App = () => <div><center><h1>Welcome to Authors Haven</h1></center></div>;
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+class App extends Component {
+simpleAction = () => {
+  this.props.simpleAction();
+}
+
+render() {
+  return (
+    <div>
+      <h1 className="title" >Welcome to Authors Haven</h1>
+      <pre>
+        {
+          JSON.stringify(this.props)
+        }
+      </pre>
+      <button className="button" onClick={this.simpleAction}>Test redux action</button>
+    </div>
+
+
+  );
+}
+}
+App.propTypes = {
+  simpleAction: PropTypes.func
+};
+
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+});
+export default connect(mapStateToProps, mapDispatchToProps)(App);

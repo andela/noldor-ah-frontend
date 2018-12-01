240cbe0e79a7dbf32e04906d870ec01b63aa0823
Chukwuemeka Ajima <chukwuemekaajima@Chukwuemekas-MacBook-Pro.local>
2018-12-01 10:02:03 +0100

    Chore(ReactProject) Setup Empty React Project:
    - Setup Webpack 4
    - Setup babel version 7
    - Configure Webpack for production and development
    - Modify npm script to run build and start server
    - Setup eslant airbnb style guide
    - Modify npm base script to run linting from the command line
    
    [Finishes #161290848]

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <div><center><h1>Welcome to Authors Haven</h1></center></div>;
+
+ReactDOM.render(<App />, document.querySelector('#app'));

68b581071ae8ac23931b7eb803999ffdd221bac5
frediflexta <fredrick.adegoke@gmail.com>
2018-12-03 18:43:26 +0100

    chore(test environment): Setup testing environment
    - setup jest environment
    - setup enzyme assertion library
    - test to see that it works
    
    [Delivers #161290852]

---
+++ src/tests/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import configureStore from 'redux-mock-store';
+import { mount } from 'enzyme';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import App from '../App';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('App component', () => {
+  it('should dispatch action', () => {
+    const initialState = {};
+    const store = mockStore(initialState);
+    const wrapper = mount(<Provider store={store}>
+      <App/>
+    </Provider>);
+
+    wrapper.find('button').simulate('click');
+    // expect(wrapper.find('h1')).toBe('Welcome to Authors Haven');
+  });
+});

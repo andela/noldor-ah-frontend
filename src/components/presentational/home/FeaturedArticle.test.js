f64dcd9f97cdb3b3777848d7d0bf07d6559ed277
Ovie Okeh <kevinokeh@gmail.com>
2018-12-17 12:53:13 +0100

    fixed failing tests

--- src/components/presentational/home/FeaturedArticle.test.js
+++ src/components/presentational/home/FeaturedArticle.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import FeaturedArticle from './featuredArticle';
+import FeaturedArticle from './FeaturedArticle';
 
 
 describe('display banner', () => {
